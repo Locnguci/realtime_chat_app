3ff0a67528754c437a676918fd171a41bae471ed
noname <noname@gmail.com>
2024-12-23 15:09:10 +0700

    reinitial project

---
+++ src/main/java/org/app/realtime_chatapp/RealtimeChatAppApplication.java
@@ -0,0 +1,13 @@
+package org.app.realtime_chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeChatAppApplication.class, args);
+    }
+
+}
